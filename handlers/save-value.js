7e6c87dd9c558e76c62c146b9899b4b561f79bf0
ShtankoDaria <daria.shtanko@gmail.com>
2020-03-04 16:41:21 +0100

    save value

--- handlers/save-value.js
+++ handlers/save-value.js
@@ -2,15 +2,29 @@ function saveValueHandler() {
   debugger;
   // read new number from user input
 let newValue =  document.getElementById('input').value;
+
+// Create Number list
+let numberList = document.getElementById('Numbery');
+const ulEl = numberlist.children[0];
+const ulEl = document.createElement("li");
+      ulEl.innerText = newValue;
+      numberList.appendChild(ulEl);
+//Create NaN list
+let notNumberList = document.getElementById('Nany');
+const ulEl = numberlist.children[0];
+const ulEl = document.createElement("li");
+      ulEl.innerText = newValue;
+      notNumberList.appendChild(ulEl);
 //Check if input is NaN or not
 let newInput = Number(newValue);
 
 if (isNaN(newInput)){
-entries.nany.push(newValue);}
+entries.nany.push(notNumberList);}
 else {
-  entries.numbery.push(newValue);
+  entries.numbery.push(numberList);
 }
  
+
 
   // log user interaction: handler name, user input, new state
   log.push({
