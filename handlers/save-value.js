4d0adb4ec007a9af97341661034326a950260b93
ShtankoDaria <daria.shtanko@gmail.com>
2020-03-04 15:18:51 +0100

    Handlers Save

--- handlers/save-value.js
+++ handlers/save-value.js
@@ -1,25 +1,21 @@
-function saveNumberHandler() {
+function saveValueHandler() {
   debugger;
   // read new number from user input
 let newValue =  document.getElementById('input').value;
-//Number(document.getElementById('input').value);
-  // read from state the data you will need for the next step
-  numbers.all.push(newValue);
-  // find the new biggest and smallest values
-  let big = Math.max(...numbers.all); 
-  let small = Math.min(...numbers.all);
+//Check if input is NaN or not
+let newInput = Number(newValue);
+
+if (isNaN(newInput)){
+entries.nany.push(newValue);}
+else {
+  entries.numbery.push(newValue);
+}
  
 
-  // update state: new biggest, new smallest, new current & save the last current
-  numbers.biggest  = big;
-  numbers.smallest = small;
-  // re-render the user interface with values stored in state
-  document.getElementById("biggest").innerText = big;
-  document.getElementById("smallest").innerText = small;
   // log user interaction: handler name, user input, new state
   log.push({
-    handler: 'save number',
+    handler: 'saveValue',
     newInput: newValue,
-    numbers: JSON.parse(JSON.stringify(numbers))
+    numbers: JSON.parse(JSON.stringify(entries))
   })
 }
