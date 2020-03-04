2770db7784e72151088aea585a36281dd4dfafde
ShtankoDaria <daria.shtanko@gmail.com>
2020-03-04 16:44:06 +0100

    handdlers

--- handlers/remove-value.js
+++ handlers/remove-value.js
@@ -1,22 +1,14 @@
 function removeValueHandler() {
   debugger;
-<<<<<<< HEAD
+
   // read new value from user input
-=======
-  // read new number from user input
->>>>>>> master
+
   let removeValue =  document.getElementById('input').value;
   removeValue.parentNode.removeChild(removeValue);
 }
 
  
 
-  // update state: new biggest, new smallest, new current & save the last current
-  numbers.biggest  = big;
-  numbers.smallest = small;
-  // re-render the user interface with values stored in state
-  document.getElementById("biggest").innerText = big;
-  document.getElementById("smallest").innerText = small;
   // log user interaction: handler name, user input, new state
   log.push({
     handler: 'save number',
