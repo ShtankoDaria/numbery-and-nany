df7951aeb42bbd5e0c8510acbe3fed94dedd31df
Daria <daria.shtanko@gmail.com>
2020-03-05 13:20:17 +0100

    more changes

--- handlers/save-value.js
+++ handlers/save-value.js
@@ -1,30 +1,26 @@
 function saveValueHandler() {
   debugger;
   // read new number from user input
-let newValue =  document.getElementById('input').value;
-
-// Create Number list
-let numberList = document.getElementById('Numbery');
-const ulEl = numberlist.children[0];
-const ulEl = document.createElement("li");
-      ulEl.innerText = newValue;
-      numberList.appendChild(ulEl);
-//Create NaN list
-let notNumberList = document.getElementById('Nany');
-const ulEl = numberlist.children[0];
-const ulEl = document.createElement("li");
-      ulEl.innerText = newValue;
-      notNumberList.appendChild(ulEl);
-//Check if input is NaN or not
-let newInput = Number(newValue);
-
-if (isNaN(newInput)){
-entries.nany.push(notNumberList);}
-else {
-  entries.numbery.push(numberList);
-}
- 
-
+  let newValue = document.getElementById("input").value;
+  let newInput = Number(newValue);
+  //Check if input is NaN or not
+  if (isNaN(newInput)) {
+    //Create NaN list
+    let notNumberList = document.getElementById("Nany");
+    const ulEl = notNumberList.children[0];
+    const ulEl = document.createElement("li");
+    entries.nany.push(notNumberList);
+    ulEl.innerText = newValue;
+    notNumberList.appendChild(ulEl);
+  } else {
+    // Create Number list
+    let numberList = document.getElementById("Numbery");
+    entries.numbery.push(numberList);
+    const ulEl = numberList.children[0];
+    const ulEl = document.createElement("li");
+    ulEl.innerText = newValue;
+    numberList.appendChild(ulEl);
+  }
 
   // log user interaction: handler name, user input, new state
   log.push({
@@ -32,3 +28,5 @@ else {
     newValue,
     entries: JSON.parse(JSON.stringify(entries))
   });
+  return true;
+}
